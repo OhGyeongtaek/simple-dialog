fcda2156c775c32f5f025d635ed9dcfc92c27356
OKT <rudxor97@naver.com>
2021-06-09 00:07:14 +0900

    타입스크립트 파일에서 자바스크립트 파일로 변경

---
+++ src/__tests__/App.spec.js
@@ -0,0 +1,15 @@
+// import App from "../App.vue";
+// import { createVuetifyWrapper } from "@/utils/VuetifyJestUtils.js";
+
+describe("App 단위테스트", () => {
+  // const wrapper = createVuetifyWrapper(App);
+  it("$alert 실행하고 팝업이 뜨는지 확인한다.", () => {
+    // const predictions = {
+    //   title: "알림",
+    //   msg: "알러트를 실행했어요.",
+    // };
+    // const results = getPopupDatas("alert");
+    expect(1).toBe(1);
+    // expect(results.title).toBe(predictions.title);
+  });
+});
